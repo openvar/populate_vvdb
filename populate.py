869c29911fb17a44f76893d9e5bd2425ea958f17
Peter-J-Freeman <peter.j.freeman@manchester.ac.uk>
2024-02-21 10:06:35 +0000

    Parallelise

--- populate.py
+++ populate.py
@@ -4,126 +4,114 @@ import VariantValidator
 from VariantValidator import update_vv_db
 import add_version_info
 import sys
-vval = VariantValidator.Validator()
 import traceback
+from concurrent.futures import ProcessPoolExecutor
 
-"""
-Notes for error fixing. 
-1. Increase transcriptVariant max cell content to 50
-
-2. Increase hgncSymbol to 50 (??? NR_036569.1) and utaSymbol to 50 (NR_036570.1 ???)
-
-2. local variable 'version' referenced before assignment
-
-3. local variable 'hgnc_symbol' referenced before assignment
-"""
+vval = VariantValidator.Validator()
 ROOT = os.path.dirname(os.path.abspath(__file__))
 
-# Check Args
-if len(sys.argv) != 3:
-    print('Too few arguments. The command required is: python populate.py False all: Where True/False dictates whether to '
-          'use the uta_transcripts_testing.txt file for testing or the default input file and All is one of all, ensembl or refseq')
-    exit()
+def check_args():
+    if len(sys.argv) != 3:
+        print('Too few arguments. The command required is: python populate.py False all: Where True/False dictates whether to '
+              'use the uta_transcripts_testing.txt file for testing or the default input file and All is one of all, ensembl or refseq')
+        exit()
 
-testing = sys.argv[1]
-transcript_set = sys.argv[2]
-print(testing)
+    testing = sys.argv[1]
+    transcript_set = sys.argv[2]
+    print(testing)
 
-# Use pgAdmin4 to download the transcripts table from UTA then replce commas for tabs and remove " characters
-if str(testing) in "False":
-    infile = os.path.join(ROOT, 'uta_transcripts.txt')
-elif str(testing) in "True":
-    infile = os.path.join(ROOT, 'uta_transcripts_testing.txt')
-else:
-    print('Argument 1 must be True or False where use the uta_transcripts_testing.txt file is True or False dictating '
-          'whether to use the uta_transcripts_testing.txt file for testing or the default input file')
-    exit()
+    if str(testing) in "False":
+        infile = os.path.join(ROOT, 'uta_transcripts.txt')
+    elif str(testing) in "True":
+        infile = os.path.join(ROOT, 'uta_transcripts_testing.txt')
+    else:
+        print('Argument 1 must be True or False where use the uta_transcripts_testing.txt file is True or False dictating '
+              'whether to use the uta_transcripts_testing.txt file for testing or the default input file')
+        exit()
 
-if str(transcript_set) not in ["all", "ensembl", "refseq"]:
-    print('Argument 2 must be all, ensembl or refseq to select a specific transcript set or not')
-    exit()
+    if str(transcript_set) not in ["all", "ensembl", "refseq"]:
+        print('Argument 2 must be all, ensembl or refseq to select a specific transcript set or not')
+        exit()
 
-# Set log and output files
+    return testing, transcript_set, infile
+
+def process_transcript(tx_id):
+    if tx_id == 'ac':
+        return
+    if not re.search('.', tx_id):
+        return
+    if '..' in tx_id:
+        return
+    if re.search('^U', tx_id):
+        return
+    if 'NG' in tx_id:
+        return
+
+    print('Updating - ' + tx_id)
+    accession = tx_id.strip()
+
+    if 'ENST' not in tx_id:
+        if transcript_set == "ensembl":
+            print("Bypass")
+            return
+        try:
+            vval.update_transcript_record(tx_id)
+            print('True')
+        except BaseException as e:
+            print('False')
+            fo.write(tx_id + '\t' + str(e) + '\n')
+            fo.flush()
+            os.fsync(fo.fileno())
+            print(tx_id + '\t' + str(e) + '\n')
+            traceback.print_exc()
+    else:
+        if transcript_set == "refseq":
+            print("Bypass")
+            return
+        try:
+            vval.update_transcript_record(tx_id, genome_build='GRCh38', test=True)
+            print('True')
+        except VariantValidator.modules.utils.DatabaseConnectionError:
+            try:
+                vval.update_transcript_record(tx_id, genome_build='GRCh37', test=True)
+                print('True')
+            except VariantValidator.modules.utils.DatabaseConnectionError as e:
+                print('False')
+                fo.write(tx_id + '\t' + str(e) + '\n')
+                fo.flush()
+                os.fsync(fo.fileno())
+                print(tx_id + '\t' + str(e) + '\n')
+                traceback.print_exc()
+            except BaseException as e:
+                print('False')
+                fo.write(tx_id + '\t' + str(e) + '\n')
+                fo.flush()
+                os.fsync(fo.fileno())
+                print(tx_id + '\t' + str(e) + '\n')
+                traceback.print_exc()
+        except BaseException as e:
+            print('False')
+            fo.write(tx_id + '\t' + str(e) + '\n')
+            fo.flush()
+            os.fsync(fo.fileno())
+            print(tx_id + '\t' + str(e) + '\n')
+            traceback.print_exc()
+
+testing, transcript_set, infile = check_args()
+
 logfile = os.path.join(ROOT, 'update_log.txt')
 fo = open(logfile, "w")
 
-# open file and loop through to populate.py
-with open(infile) as tx_data:
-    for tx_line in tx_data:
-        tx_cells = tx_line.split('\t')
-        tx_id = tx_cells[0]
-
-        # populate
-        if tx_id == 'ac':
-            continue
-        if not re.search('.', tx_id):
-            continue
-        if '..' in tx_id:
-            continue
-        if re.search('^U', tx_id):
-            continue
-        if 'NG' in tx_id:
-            continue
-
-        print('Updating - ' + tx_id)
-        accession = tx_id.strip()
-        # Look for the accession in our database
-        # Connect to database and send request
-        if 'ENST' not in tx_id:
-            if transcript_set == "ensembl":
-                print("Bypass")
-                continue
-            try:
-                vval.update_transcript_record(tx_id)
-                print('True')
-            except BaseException as e:
-                print('False')
-                fo.write(tx_id + '\t' + str(e) + '\n')
-                fo.flush()
-                os.fsync(fo.fileno())
-                print(tx_id + '\t' + str(e) + '\n')
-                traceback.print_exc()
-        else:
-            if transcript_set == "refseq":
-                print("Bypass")
-                continue
-            try:
-                vval.update_transcript_record(tx_id, genome_build='GRCh38', test=True)
-                print('True')
-            except VariantValidator.modules.utils.DatabaseConnectionError:
-                try:
-                    vval.update_transcript_record(tx_id, genome_build='GRCh37', test=True)
-                    print('True')
-                except VariantValidator.modules.utils.DatabaseConnectionError as e:
-                    print('False')
-                    fo.write(tx_id + '\t' + str(e) + '\n')
-                    fo.flush()
-                    os.fsync(fo.fileno())
-                    print(tx_id + '\t' + str(e) + '\n')
-                    traceback.print_exc()
-                except BaseException as e:
-                    print('False')
-                    fo.write(tx_id + '\t' + str(e) + '\n')
-                    fo.flush()
-                    os.fsync(fo.fileno())
-                    print(tx_id + '\t' + str(e) + '\n')
-                    traceback.print_exc()
-            except BaseException as e:
-                print('False')
-                fo.write(tx_id + '\t' + str(e) + '\n')
-                fo.flush()
-                os.fsync(fo.fileno())
-                print(tx_id + '\t' + str(e) + '\n')
-                traceback.print_exc()
+with open(infile) as tx_data, ProcessPoolExecutor(max_workers=4) as executor:
+    tx_ids = (tx_line.split('\t')[0] for tx_line in tx_data)
+    executor.map(process_transcript, tx_ids)
 
 fo.close()
 
 if str(testing) in "False":
-    # Update everything else
     update_vv_db.update()
     add_version_info.update_version()
 
-# Finish
 print("UPDATE COMPLETE: Check update_log for failed transcripts and correct")
 
 # <LICENSE>

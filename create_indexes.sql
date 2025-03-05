fe436865bd24ba1dfbe6cb3bd80dfaeb2540bd42
Peter-J-Freeman <peter.j.freeman@manchester.ac.uk>
2025-03-05 16:24:23 +0000

    another index

--- create_indexes.sql
+++ create_indexes.sql
@@ -1,4 +1,5 @@
 ALTER TABLE stableGeneIds ADD INDEX hgnc_id_index (hgnc_id);
+ALTER TABLE stableGeneIds ADD INDEX hgnc_id_index (hgnc_id);
 ALTER TABLE transcript_info ADD INDEX refSeqID_index (refSeqID);
 ALTER TABLE transcript_info ADD INDEX utaSymbol_index (utaSymbol);
 ALTER TABLE transcript_info ADD INDEX hgncSymbol_index_index (hgncSymbol);
